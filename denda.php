fe82ab7571c5f26e6bc3609ac76e58c14ccec37e
RivaroFarrelino <rivfarrelino@gmail.com>
2023-12-14 21:04:19 +0700

    Add ajax

--- denda.php
+++ denda.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="dendastyles.css">
     <title>Denda</title>
@@ -33,20 +34,43 @@ if ($dataDenda !== false) {
             </thead>
             <tbody>';
     
-    foreach ($dataDenda as $row) {
-        echo '<tr>
-                <th scope="row">' . $row['id_denda'] . '</th>
-                <td>' . $row['id_pengembalian'] . '</td>
-                <td>' . $row['total_denda'] . '</td>
-                <td>' . ($row['status_bayar'] == 1 ? 'Sudah Bayar' : 'Belum Bayar') . '</td>
-                <td><button onclick="bayarDenda(' . $row['id_denda'] . ')" class="btn btn-primary">Bayar Denda</button></td>
-            </tr>';
-    }
+            foreach ($dataDenda as $row) {
+              if ($row['status_bayar'] == 0) {
+                  echo '<tr id="row_' . $row['id_denda'] . '">
+                          <th scope="row">' . $row['id_denda'] . '</th>
+                          <td>' . $row['id_pengembalian'] . '</td>
+                          <td>' . $row['total_denda'] . '</td>
+                          <td>' . ($row['status_bayar'] == 1 ? 'Sudah Bayar' : 'Belum Bayar') . '</td>
+                          <td><button onclick="bayarDenda(' . $row['id_denda'] . ')" class="btn btn-primary">Bayar Denda</button></td>
+                      </tr>';
+              }
+          }
+          
 
     echo '</tbody></table>';
 } else {
     echo '<p>Tidak ada data denda yang perlu ditampilkan.</p>';
 }
 ?>
+
+<script>
+function bayarDenda(idDenda) {
+  console.log("Button clicked with ID Denda: " + idDenda);
+  $.ajax({
+        type: "POST",
+        url: "Controller/bayarDenda.php",
+        data: { id_denda: idDenda },
+        success: function(response) {
+            console.log(response); // Tampilkan respons di konsol
+            alert(response);
+            $("#row_" + idDenda).hide();
+        },
+        error: function(xhr, status, error) {
+            console.log(xhr.responseText); // Tampilkan pesan kesalahan di konsol
+            alert("Gagal memperbarui status bayar. Error: " + error);
+        }
+    });
+}
+</script>
 </body>
 </html>
