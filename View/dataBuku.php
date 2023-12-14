09c7359d6841b716fe20b176dbbe950293e018e1
Theoraditya <theoraditya195@gmail.com>
2023-12-14 21:12:00 +0700

    Create dataBuku.php

---
+++ View/dataBuku.php
@@ -0,0 +1,19 @@
+<?php
+$bukuModel = new m_dataBuku();
+$buku = $bukuModel->tampilBuku();
+
+include_once "View/dataBuku.php";
+foreach ($buku as $row) {
+    echo "<tr>",
+        "<td>{$row['id_buku']}</td>",
+        "<td>{$row['judul']}</td>",
+        "<td>{$row['pengarang']}</td>",
+        "<td>{$row['penerbit']}</td>",
+        "<td>{$row['ISBN']}</td>",
+        "<td>{$row['tahun']}</td>",
+        "<td>{$row['stok']}</td>",
+        "</tr>";
+}
+
+echo "</tbody>";
+echo "</table>";
