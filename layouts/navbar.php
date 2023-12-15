6e716fc85e5a24afa21c0f2b7887bd0ed792c91c
RivaroFarrelino <rivfarrelino@gmail.com>
2023-12-15 17:21:23 +0700

    bosen

---
+++ layouts/navbar.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="navbarstyles.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="../index.php"><img src="../assets/images/icon.png" alt="icon-perpus" style="margin-top: 0.2rem"></a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Anggota</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Data Buku</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Peminjaman</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../">Pengembalian</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../denda.php">Denda</a>
+                </li>
+                <a href="../profile.php"><img src="../assets/images/account-icon.png" alt="account-icon" style="width: 2rem; height: 2rem; margin-top: 0.2rem;"></a>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
