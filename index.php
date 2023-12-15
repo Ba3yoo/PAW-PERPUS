207cbc9c7534e305e51c9407950effef55100f67
Ba3yoo <115632689+Ba3yoo@users.noreply.github.com>
2023-12-15 22:48:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,15 +35,15 @@ include_once "layouts/navbar.php";
 
 <h2 style="text-align: center;">Selamat datang di Sistem Perpustakaan</h2>
 
-<div id="carouselExampleDark" class="carousel carousel-dark slide" style="margin-left: 60px; margin-right: 60px; margin-bottom: 30px; width: 50rem; height: 50rem;">
+<!-- <div id="carouselExampleDark" class="carousel carousel-dark slide" style="margin-left: 60px; margin-right: 60px; margin-bottom: 30px; width: 40rem; height: 50rem;">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner" style="border-radius: 10px;">
+  <div class="carousel-inner" style="border-radius: 10px; height:30rem">
     <div class="carousel-item active" data-bs-interval="10000">
-      <img src="assets/images/home-1.jpg" class="d-block w-100 h-50" alt="slide-1">
+      <img src="assets/images/home-1.jpg" class="d-block w-100 h-50" alt="slide-1" style="height:100%;">
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
@@ -72,7 +72,53 @@ include_once "layouts/navbar.php";
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+</div> -->
+
+<div id="carouselExampleDark" class="carousel carousel-dark slide" style="height:40rem;">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="assets/images/home-1.jpg" class="d-block w-100" alt="slide-1" style="height:40rem;">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="card text-bg-dark">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="assets/images/home-2.jpg" class="d-block w-100" alt="slide-2" style="height:40rem;">
+      <div class="carousel-caption d-none d-md-block">
+      <div class="card text-bg-dark">
+        <h5>Second slide label</h5>  
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/home-3.png" class="d-block w-100" alt="slide-3" style="height:40rem;">
+      <div class="carousel-caption d-none d-md-block">
+      <div class="card text-bg-dark">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
+<br>
 </body>
 
 </html>
