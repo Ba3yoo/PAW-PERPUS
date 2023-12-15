2fc73b594cfb1e734f267997fd7e9b80f956b360
nurshineds <153358097+nurshineds@users.noreply.github.com>
2023-12-15 16:54:44 +0700

    ubah array key broh

--- View/tabelAnggota.php
+++ View/tabelAnggota.php
@@ -6,14 +6,14 @@ include_once "View/tabelAnggota.php";
 foreach ($anggota as $row) {
     echo "<tr>",
         "<td>{$row['id_anggota']}</td>",
-        "<td>{$row['nama_lengkap']}</td>",
+        "<td>{$row['nama']}</td>",
         "<td>{$row['tgl_daftar']}</td>";
         if($row['status_aktif'] == 0){
             echo "<td>Tidak Aktif</td>";
         } else if($row['status_aktif'] == 1){
             echo "<td>Aktif</td>";
         }
-        echo "<td><a href='editAnggota.php?edit={$row['id_anggota']}?nama={$row['nama_lengkap']}?status={$row['status_aktif']}' class='btn btn-primary'>Edit</a></td>";
+        echo "<td><a href='editAnggota.php?edit={$row['id_anggota']}?nama={$row['nama']}?status={$row['status_aktif']}' class='btn btn-primary'>Edit</a></td>";
         echo "<td><a href='controller/anggotaController.php?delete={$row['id_anggota']}' class='btn btn-secondary'>Delete</a></td>";
         echo "</tr>";
 }
