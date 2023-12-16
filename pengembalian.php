74466a36a6a562edaa80d404101b6b12dbb061ae
FiegoG <fiegotriwanda@gmail.com>
2023-12-16 09:52:56 +0700

    hilangkan jejak gpt

--- pengembalian.php
+++ pengembalian.php
@@ -4,17 +4,10 @@ include 'Controller/c_pengembalian.php';
 $load = new c_pengembalian;
 $load->tableView();
 
-// Pastikan $_GET['id'] ada dan merupakan angka (untuk keamanan)
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     $id_peminjaman = $_GET['id'];
-
-    // Sekarang Anda memiliki $id_peminjaman untuk digunakan dalam pemanggilan method atau query lainnya
-
-    // Misalnya, jika ingin memanggil method `tambahPengembalian` dari c_pengembalian.php dengan ID tersebut:
     include_once 'Controller/c_pengembalian.php';
 
     $controller = new c_pengembalian();
     $controller->tambahPengembalian($id_peminjaman);
-    
-    // Atau Anda dapat melakukan hal lain sesuai kebutuhan aplikasi Anda di sini
 }
