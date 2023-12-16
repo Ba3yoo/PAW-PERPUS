c8701eabe9f734aa62db5563600e21bf6232955e
Ba3yoo <115632689+Ba3yoo@users.noreply.github.com>
2023-12-16 09:05:52 +0700

    Update peminjaman.php

--- peminjaman.php
+++ peminjaman.php
@@ -12,7 +12,7 @@ $controller = new PinjamController();
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="icon" href="/assets/images/icon.png" type="image/x-icon">
-  <title>Document</title>
+  <title>Peminjaman</title>
 </head>
 
 <body>
