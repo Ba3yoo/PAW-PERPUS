c8211f607adefdcc178c47218f76d2df458684a2
Ba3yoo <115632689+Ba3yoo@users.noreply.github.com>
2023-12-16 10:58:35 +0700

    Update PinjamTabel.php

--- View/PinjamTabel.php
+++ View/PinjamTabel.php
@@ -1,13 +1,15 @@
 <?php
+$i = 1;
 foreach ($borrow as $borrow) {
         echo "<tr>",
-        "<td>$borrow[id_peminjaman]</td>",
+        "<td>$i</td>",
         "<td>$borrow[judul]</td>",
         "<td>$borrow[nama]</td>",
         "<td>$borrow[tgl_pinjam]</td>",
         "<td>$borrow[tgl_kembali]</td>",
         '<td><a class="btn btn-secondary" href="pengembalian.php?id='. "$borrow[id_peminjaman]" .'">Kembalikan</a></td>';
         "</tr>";
+        $i++;
 }
 echo "</tbody>";
 echo "</table>";
